565bf30245e416823859d290972327032cd697d9
Shiv <sivaprasad.tr@siemens.com>
2020-10-20 17:27:38 +0530

    Rebased

--- actions.py
+++ actions.py
@@ -10,54 +10,73 @@ import json
 import sendemail
 
 class ActionSearchRestaurants(Action):
-	def name(self):
-		return 'action_search_restaurants'
-		
-	def run(self, dispatcher, tracker, domain):
-		try:
-			config={ "user_key":"f4924dc9ad672ee8c4f8c84743301af5"}
-			zomato = zomatopy.initialize_app(config)
-			loc = tracker.get_slot('location')
-			cuisine = tracker.get_slot('cuisine')
-			location_detail=zomato.get_location(loc, 1)
+    def name(self):
+        return 'action_search_restaurants'
+        
+    def run(self, dispatcher, tracker, domain):
+        try:
+            config={ "user_key":"7a65e3c0d290c885f1754d52235da690"}
+            zomato = zomatopy.initialize_app(config)
+            loc = tracker.get_slot('location')
+            cuisine = tracker.get_slot('cuisine')
+            location_detail=zomato.get_location(loc, 1)
 
-			d1 = json.loads(location_detail)
-			lat=d1["location_suggestions"][0]["latitude"]
-			lon=d1["location_suggestions"][0]["longitude"]
-			cuisines_dict={'bakery':5,'chinese':25,'cafe':30,'italian':55,'biryani':7,'north indian':50,'south indian':85}
+            d1 = json.loads(location_detail)
+            lat=d1["location_suggestions"][0]["latitude"]
+            lon=d1["location_suggestions"][0]["longitude"]
+            cuisines_dict={'bakery':5,'chinese':25,'cafe':30,'italian':55,'biryani':7,'north indian':50,'south indian':85}
 
-			results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
-			d = json.loads(results)
+            results=zomato.restaurant_search("", lat, lon, str(cuisines_dict.get(cuisine)), 5)
+            d = json.loads(results)
 
-			response="Showing you top rated restaurants..\n"
-			if d['results_found'] == 0:
-				response= "Sorry! No restaurants found"
-			else:
-				for i, restaurant in enumerate(d['restaurants']):
-					response=response + str(i + 1) + ". " + restaurant['restaurant']['name'] + \
-						" in " + restaurant['restaurant']['location']['address'] + \
-						" has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
+            response="Showing you top rated restaurants..\n"
+            if d['results_found'] == 0:
+                response= "Sorry! No restaurants found"
+            else:
+                for i, restaurant in enumerate(d['restaurants']):
+                    response=response + str(i + 1) + ". " + restaurant['restaurant']['name'] + \
+                        " in " + restaurant['restaurant']['location']['address'] + \
+                        " has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
 
-			response = response + "\n\n"
+            response = response + "\n\n"
 
-			dispatcher.utter_message(response)
+            dispatcher.utter_message(response)
 
-			return [SlotSet('location',loc), SlotSet('error', False)]
-		except:
-			dispatcher.utter_message("Something went wrong. Please try again later.")
-			return [SlotSet('error', True)]
+            return [SlotSet('location',loc), SlotSet('error', False)]
+        except:
+            dispatcher.utter_message("Something went wrong. Please try again later.")
+            return [SlotSet('error', True)]
 
 class ActionSendEmail(Action):
-	def name(self):
-		return "action_send_email"
-	
-	def run(self, dispatcher, tracker, domain):
-		receiver_address = tracker.get_slot('email')
-		loc = tracker.get_slot('location')
-		cuisine = tracker.get_slot('cuisine')
+    def name(self):
+        return "action_send_email"
+    
+    def run(self, dispatcher, tracker, domain):
+        receiver_address = tracker.get_slot('email')
+        loc = tracker.get_slot('location')
+        cuisine = tracker.get_slot('cuisine')
 
-		sendemail.send(receiver_address, loc, cuisine, subject='Restaurant Search Bot')
+        print('sending email')
 
-		dispatcher.utter_message("Sent")
+        sendemail.send(receiver_address, loc, cuisine, subject='Restaurant Search Bot')
 
-		return [SlotSet('email', receiver_address)]
+        dispatcher.utter_message("Sent")
+
+        return [SlotSet('email', receiver_address)]
+
+
+class ActionCheckCitySupport(Action):
+    def name(self):
+        return "action_check_city_support"
+    
+    def run(self, dispatcher, tracker, domain):
+        loc = tracker.get_slot('location')
+
+        with open("data\lookups\location.txt") as openfile:
+            for line in openfile:
+                if(line.rstrip("\n") == loc):
+                    return [SlotSet('city_support', True)]
+
+        return [SlotSet('city_support', False)]
+
+
